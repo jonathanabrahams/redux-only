3d286d870f1d5c093b6c2a30803f62eb29a78b56
jonathanabrahams <jonathanabrahams@gmail.com>
2018-05-12 15:48:25 +0200

    Add middleware logger

--- client/counter.js
+++ client/counter.js
@@ -1,14 +1,27 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import DevTools from './components/DevTools';
 
+const logger = store => next => action => {
+    console.group(action.type);
+    console.log("LOG:ACTION",action);
+    action = {...action, before: 'before'};
+    let result = next(action);
+    result = {...result, after: 'after' };
+    console.log("LOG:RESULT", result);
+    console.groupEnd(action.type);
+    return result;
+}
+
 const enhancer = compose(
     // Middleware for development
+    applyMiddleware(logger),
     //Required!
     DevTools.instrument()
 )
 
 //Reducers
 function counter(state = { value: 0 }, action) {
+    console.log('REDUCE',action);
     switch (action.type) {
         case 'INC':
             return { ...state, value: state.value + 1 };
@@ -35,7 +48,7 @@ function doDecrement() {
 let store = createStore(counter, {value:0}, enhancer);
 
 //Subscriber
-store.subscribe(() => console.log(store.getState()));
+store.subscribe(() => console.log("SUBSCRIBE", store.getState()));
 
 // Dispatch action
 store.dispatch(doIncrement());
